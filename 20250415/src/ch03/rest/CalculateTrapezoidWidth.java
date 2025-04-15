477efac826dd491ab6f71d4c772f9002e6714eb3
syann97 <shallom75321@gmail.com>
2025-04-15 16:04:26 +0900

    20250414 심화 과제

---
+++ 20250415/src/ch03/rest/CalculateTrapezoidWidth.java
@@ -0,0 +1,11 @@
+package ch03.rest;
+
+public class CalculateTrapezoidWidth {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int h = 7;
+
+        System.out.println(((a + b) / 2) * h);
+    }
+}
