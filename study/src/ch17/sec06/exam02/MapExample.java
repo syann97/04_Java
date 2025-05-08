85b926ab8a09303cd6e87835ae57c07a7b32b697
syann97 <shallom75321@gmail.com>
2025-05-08 15:25:26 +0900

    20250508 심화 문제

---
+++ study/src/ch17/sec06/exam02/MapExample.java
@@ -0,0 +1,18 @@
+package ch17.sec06.exam02;
+
+import java.util.Arrays;
+
+public class MapExample {
+    public static void main(String[] args) {
+        int[] intArray = { 1, 2, 3, 4, 5};
+
+        Arrays.stream(intArray)
+                .mapToDouble(num -> num)
+                .forEach(System.out::println);
+        System.out.println();
+
+
+        Arrays.stream(intArray)
+                .forEach(System.out::println);
+    }
+}
---
+++ study/src/ch17/sec06/exam03/FlatMappingExample.java
@@ -0,0 +1,32 @@
+package ch17.sec06.exam03;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FlatMappingExample {
+    public static void main(String[] args) {
+
+        List<String> list1 = new ArrayList< >();
+        list1.add("this is java");
+        list1.add("i am a best developer");
+        //문장 스트림을 단어 스트림으로 변환 후 출력
+        list1.stream()
+                .flatMap(str -> Arrays.stream(str.split(" ")))
+                .forEach(System.out::println);
+        System.out.println();
+
+
+        List<String> list2 = Arrays.asList("10, 20, 30", "40, 50");
+        //문자열 숫자 목록 스트림을 숫자 스트림으로 변환 후 출력
+        list2.stream()
+                .flatMapToInt(data -> {
+                    String[] strArr = data.split(",");
+                    int[] intArr = new int[strArr.length];
+                    for (int i = 0; i < strArr.length; i++) {
+                        intArr[i] = Integer.parseInt(strArr[i].trim());
+                    }
+                    return Arrays.stream(intArr);
+                })
+                .forEach(System.out::println);
+    }
+}
